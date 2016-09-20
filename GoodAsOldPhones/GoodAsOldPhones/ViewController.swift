df34d62049df454231469d5a3aaa987d59f417b8
CHEN SU <martinsuchen3557@gmail.com>
2016-09-19 21:04:57 -0400

    Project creation.

---
+++ GoodAsOldPhones/GoodAsOldPhones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by CHEN SU on 9/19/16.
+//  Copyright © 2016 chensuworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
