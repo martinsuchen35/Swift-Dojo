9b49e720e05ccd45f95a49feb458ab4635f78239
Chen Su <martinsuchen3557@gmail.com>
2016-10-02 17:54:00 -0400

    Use scroll view.

---
+++ GoodAsOldPhones/GoodAsOldPhones/ContactViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ContactViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Chen Su on 10/2/16.
+//  Copyright © 2016 chensuworks. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(scrollView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSize(width: 375, height: 800)
+    }
+
+}
